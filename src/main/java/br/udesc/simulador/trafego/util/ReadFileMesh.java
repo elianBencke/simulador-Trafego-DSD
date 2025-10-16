855f5e78d001e1a029c5d83df0531290216fbc1e
elianBencke <168542697+elianBencke@users.noreply.github.com>
2025-10-16 17:37:02 -0300

    Ajustes finais

--- src/main/java/br/udesc/simulador/trafego/util/ReadFileMesh.java
+++ src/main/java/br/udesc/simulador/trafego/util/ReadFileMesh.java
@@ -10,17 +10,8 @@ public class ReadFileMesh {
     public static int[][] generateRoadMesh(File file) throws Exception {
         List<String> fileLines = Files.readAllLines(Path.of(file.getPath()));
 
-<<<<<<< HEAD
-        String rowStr = fileLines.get(0).trim();
-        int meshRows = Integer.parseInt(rowStr);
-
-        String colStr = fileLines.get(1).trim();
-        int meshColumns = Integer.parseInt(colStr);
-
-=======
         int meshRows = Integer.parseInt(fileLines.get(0).trim());
         int meshColumns = Integer.parseInt(fileLines.get(1).trim());
->>>>>>> 61bb0818a490d984dbc7207e373a287c52d53da6
         int[][] roadMesh = new int[meshRows][meshColumns];
 
         fileLines = fileLines.subList(2, fileLines.size());
@@ -29,22 +20,11 @@ public class ReadFileMesh {
             String[] columns = fileLines.get(row).trim().split("\\s+");
 
             for (int column = 0; column < columns.length; column++) {
-<<<<<<< HEAD
-                String cellValue = columns[column].trim();
-
-                if (!cellValue.isEmpty()) {
-                    try {
-                        roadMesh[row][column] = Integer.parseInt(cellValue);
-                    } catch (NumberFormatException e) {
-                        throw new Exception("O valor '" + cellValue + "' não é um inteiro válido na malha.", e);
-                    }
-=======
                 if (columns[column].isEmpty()) {
                     continue;
                 }
                 if (column < meshColumns) {
                     roadMesh[row][column] = Integer.parseInt(columns[column]);
->>>>>>> 61bb0818a490d984dbc7207e373a287c52d53da6
                 }
             }
         }
